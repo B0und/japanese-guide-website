9a48dd0c679bf67a8e85a63978f3969253754219
b0und <f1arenova1@gmail.com>
2021-06-16 20:23:51 +0300

    added sidebars

--- js/toggle_navbar.js
+++ js/toggle_navbar.js
@@ -1,5 +1,8 @@
+// delegate event from footer to child element, because it was loaded with .load and the code above doesnt work in this case
+// https://api.jquery.com/on/#direct-and-delegated-events
 $(function () {
-    $("#navbar").click(function () {
-      $("#navbar__menu").toggleClass("show");
-    });
+  $(".navbar").on("click", ".navbar__hamburger", function () {
+    $(".navbar__menu").toggleClass("mobile-nav");
+    $(this).toggleClass("is-active");
   });
+});
