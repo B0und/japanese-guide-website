65cdb12d9cb64e35e33a2ea7db355d9aa9cfc834
b0und <f1arenova1@gmail.com>
2021-06-20 11:26:21 +0300

    change colors, fix js

--- js/highlight_active_page.js
+++ js/highlight_active_page.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   var $page = location.pathname;
-  console.log($page);
   const matches = document.querySelectorAll(
     "a.navbar__menu--links, a.sidebar-container__links"
   );
   matches.forEach(function (link) {
     var $link = "/" + link.getAttribute("href");
-    if ($link == $page) {
+    if ($page.endsWith($link)) {
       link.classList.add("active-page");
     } else {
       link.classList.remove("active-page");
